415f761413c7ddba6c5c55573ef838118ea9e056
Ashish <ashishrath1997@gmail.com>
2024-01-13 00:19:03 +0530

    utils functions created for error and response

--- src/index.js
+++ src/index.js
@@ -6,6 +6,19 @@ dotenv.config({
 })
 
 connect_DB()
+.then(() => {
+    app.on("Error ", (error) => {
+        console.log("ERROR: ", error)
+        throw error
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is listening on port ${process.env.port}`)
+    })
+})
+.catch((error) => {
+    console.log("ERROR: MONGO_DB connection failed - ", error)
+}) 
 
 // import express from "express";
 
